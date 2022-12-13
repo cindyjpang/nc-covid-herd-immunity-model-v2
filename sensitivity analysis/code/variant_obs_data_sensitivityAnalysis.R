a5c103dd7beb5bcd14beb04c542ad2ebcfceedb5
Cindy Pang <77863668+cindyjpang@users.noreply.github.com>
2022-12-13 17:13:15 -0500

    output files asjdkfl;sa

--- sensitivity analysis/code/variant_obs_data_sensitivityAnalysis.R
+++ sensitivity analysis/code/variant_obs_data_sensitivityAnalysis.R
@@ -27,57 +27,123 @@ library(zoo)
 library(ggpmisc)
 
 
-nc_dat <- read.csv("./exported data/sensitivity analysis data export/SIR Initial Condition/sir_initCond_death_inf_vacc_obs_imm.csv")
-nc_dat <- filter(nc_dat, I!=0)[, c('COUNTY', 'DATE', 'N', 'I', 'R', 'S')]
-nc_dat$DATE <- as.Date(nc_dat$DATE)
+
+scenarios <- c("S1/sir_initCond_cdc_case_vacc_obs_imm.csv", "S2/sir_initCond_cdc_case_vacc_obs_imm_up.csv", "S3/sir_initCond_cdc_case_vacc_obs_imm_lower.csv", "S4/sir_initCond_death_inf_vacc_obs_imm.csv", "S5/sir_initCond_death_inf_vacc_obs_imm_up.csv", "S6/sir_initCond_death_inf_vacc_obs_imm_lower.csv")
+
+for(scenario in scenarios){
+  read_file_path <- paste0("./sensitivity analysis/outputs/", scenario)
+  print(read_file_path)
+  
+  nc_dat <- read.csv(read_file_path)
+  nc_dat <- filter(nc_dat, I!=0)[, c('COUNTY', 'DATE', 'N', 'I', 'R', 'S')]
+  nc_dat$DATE <- as.Date(nc_dat$DATE)
+  
+  
+  ## Set minimum date to May 1st 2021
+  #start_search <- as.Date("2021-04-01")
+  nc_dat <- nc_dat %>%
+    group_by(COUNTY)%>%
+    mutate(days_since_start = as.numeric(difftime(DATE, as.Date("2020-01-01"), units = c('days'))))%>%
+    filter(COUNTY != "Missing")
+  
+  
+  counties <- unique(nc_dat$COUNTY)
+  smoothed_inf <- c()
+  
+  # look at the smoothed curves 
+  for(county in counties){
+    df <- filter(nc_dat, COUNTY == county)
+    loessMod_op <- loess(I ~ days_since_start, data=df, span=0.3)
+    smoothed_op <- predict(loessMod_op)
+    smoothed_inf <- append(smoothed_inf, smoothed_op)
+    print(ggplot(data = df)+geom_col(aes(DATE, I), alpha=0.7) + geom_line(aes(DATE, smoothed_op), col = 'red')+ggtitle(county))
+  }
+  nc_dat$smoothed_inf <- smoothed_inf
+  nc_dat <- nc_dat %>% group_by(COUNTY) %>% mutate(minima = ggpmisc:::find_peaks(-smoothed_inf, na.rm = TRUE))
+  
+  #sanity check - plot the minimum points via ggplot
+  for(county in counties){
+    df <- filter(nc_dat, COUNTY == county)
+    print(paste(county, sep=" : ",df$DATE[which(df$minima == TRUE)] ))
+    c <- df$DATE[which(df$minima == TRUE)]
+    print(c)
+    q <- ggplot(df) + geom_col(aes(DATE, I), alpha = 0.7) +geom_line(aes(DATE, smoothed_inf), col = 'red') + ggtitle(county)
+    print(q + geom_vline(xintercept = c))
+  }
+  
+  
+  # test for the best minima, screens out noise, out of the three which has the min(smoothed_inf)
+  test <- nc_dat %>% group_by(COUNTY)%>%filter(minima == TRUE)%>%summarise(min_dates = tail(DATE,3))
+  test <- test[test$min_dates >= as.Date('2021-04-01'), ]
+  test <- test %>% add_count(COUNTY) # filter the dates which are strictly delta vs omicron waves
+  
+  delta_dates <- test %>%
+    group_by(COUNTY)%>%
+    summarize(delta_start = min(min_dates))
+  
+  missing_counties <- setdiff(counties, delta_dates$COUNTY)
+  
+  # number of times to replicate start date
+  rep_num <- length(missing_counties)
+  
+  # build dataframe for binding
+  temp_df <- data.frame(COUNTY = missing_counties, 
+                        delta_start = rep(as.Date("2021-05-01"), rep_num))
+  
+  delta_dates <- rbind(delta_dates, temp_df)
+  
+  delta_obs_dat <- data.frame()
+  for(county in counties){
+    df <- filter(delta_dates, COUNTY == county)
+    delta_start <- df$delta_start
+    df_delta <- nc_dat %>%
+      filter(COUNTY == county & DATE >= delta_start)%>%
+      select(COUNTY, DATE, N, I, R, S)
+    df_delta$days <- lapply(df_delta$DATE, function(x) {return(difftime(x, delta_start, units=c('days')))})
+    df_delta$days <- as.numeric(unlist(df_delta$days))
+    
+    delta_obs_dat <- rbind(delta_obs_dat, df_delta)
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  path_str <- "./sensitivity analysis/outputs/"
+  if(scenario == "S1/sir_initCond_cdc_case_vacc_obs_imm.csv"){
+      write_path <- paste0(path_str, "S1/delta_obs_dat_cdc_case_vacc_obs_imm.xlsx")
+      write_xlsx(delta_obs_dat, write_path)
+  }else if(scenario == "S2/sir_initCond_cdc_case_vacc_obs_imm_up.csv"){
+    write_path <- paste0(path_str, "S2/delta_obs_dat_cdc_case_vacc_obs_imm_up.xlsx")
+    write_xlsx(delta_obs_dat, write_path)
+  }else if(scenario == "S3/sir_initCond_cdc_case_vacc_obs_imm_lower.csv"){
+    write_path <- paste0(path_str, "S3/delta_obs_dat_cdc_case_vacc_obs_imm_lower.xlsx")
+    write_xlsx(delta_obs_dat, write_path)
+  }else if(scenario == "S4/sir_initCond_death_inf_vacc_obs_imm.csv"){
+    write_path <- paste0(path_str, "S4/delta_obs_dat_death_inf_vacc_obs_imm.xlsx")
+    write_xlsx(delta_obs_dat, write_path)
+  }else if(scenario == "S5/sir_initCond_death_inf_vacc_obs_imm_up.csv"){
+    write_path <- paste0(path_str, "S5/delta_obs_dat_death_inf_vacc_obs_imm_up.xlsx")
+    write_xlsx(delta_obs_dat, write_path)
+  }else{
+    write_path <- paste0(path_str, "S6/delta_obs_dat_death_inf_vacc_obs_imm_lower.xlsx")
+    write_xlsx(delta_obs_dat, write_path)
+    
+  }
+  
+  
+  
+  
+  }
 
 
-## Set minimum date to May 1st 2021
-start_search <- as.Date("2021-04-01")
-nc_dat <- nc_dat %>%
-  #filter(DATE >= start_search)%>%
-  group_by(COUNTY)%>%
-  mutate(days_since_start = as.numeric(difftime(DATE, start_search, units = c('days'))))
-#)
-
-# counties <- unique(nc_dat$COUNTY)
-# 
-# for(county in counties){
-#   df <- filter(nc_dat, COUNTY == county)
-#   print(ggplot(data = df)+geom_col(aes(DATE, I), alpha=0.7) + geom_line(aes(DATE, mav), col = 'red')+ggtitle(county))
-# }
-
-counties <- unique(nc_dat$COUNTY)
-smoothed_inf <- c()
-
-# look at the smoothed curves 
-for(county in counties){
-  df <- filter(nc_dat, COUNTY == county)
-  loessMod_op <- loess(I ~ days_since_start, data=df, span=0.3)
-  smoothed_op <- predict(loessMod_op)
-  smoothed_inf <- append(smoothed_inf, smoothed_op)
-  print(ggplot(data = df)+geom_col(aes(DATE, I), alpha=0.7) + geom_line(aes(DATE, smoothed_op), col = 'red')+ggtitle(county))
-}
-nc_dat$smoothed_inf <- smoothed_inf
-nc_dat <- nc_dat %>% group_by(COUNTY) %>% mutate(minima = ggpmisc:::find_peaks(-smoothed_inf, na.rm = TRUE))
-#nc_dat[nc_dat$COUNTY == 'Graham' & nc_dat$minima == TRUE & nc_dat$days_since_start == 462, "minima"] <- FALSE
-
-#sanity check - plot the minimum points via ggplot
-for(county in counties){
-  df <- filter(nc_dat, COUNTY == county)
-  print(paste(county, sep=" : ",df$DATE[which(df$minima == TRUE)] ))
-  c <- df$DATE[which(df$minima == TRUE)]
-  print(c)
-  q <- ggplot(df) + geom_col(aes(DATE, I), alpha = 0.7) +geom_line(aes(DATE, smoothed_inf), col = 'red') + ggtitle(county)
-  print(q + geom_vline(xintercept = c))
-}
-
-
-# test for the best minima, screens out noise, out of the three which has the min(smoothed_inf)
-test <- nc_dat %>% group_by(COUNTY)%>%filter(minima == TRUE)%>%summarise(min_dates = tail(DATE,2))
-test <- test[test$min_dates >= as.Date('2021-04-01'), ]
-test <- test %>% add_count(COUNTY) # filter the dates which are strictly delta vs omicron waves
-View(test)
 
 
 
@@ -86,70 +152,5 @@ View(test)
 
 
 
+#write_xlsx(delta_obs_dat, "./sensitivity analysis/outputs/S1/delta_obs_dat_cdc_case_vacc_obs_imm_up.xlsx")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# plot county data
-
-# counties <- unique(nc_dat$COUNTY)
-# smoothed_inf <- c()
-# smoothed_df <- data.frame()
-# # look at the smoothed curves 
-# for(county in counties){
-#   df <- filter(nc_dat, COUNTY == county)
-#   if(length(df$COUNTY) > 4){
-#     poly_df <- lm(df$I ~ poly(df$days_since_start, 4))
-#     
-#     # loessMod_op <- loess(I ~ days_since_start, data=df, span=1)
-#     smoothed_op <- poly_df$fitted.values
-#     smoothed_inf <- append(smoothed_inf, smoothed_op)
-#     print(ggplot(data = df)+geom_col(aes(DATE, I), alpha=0.7) + geom_line(aes(DATE, smoothed_op), col = 'red')+ggtitle(county))
-#   }else{
-#     smoothed_inf <- append(smoothed_inf, df$I)
-#   } 
-#     
-# }
-# 
-# nc_dat$smoothed_inf <- smoothed_inf
-#nc_dat <- nc_dat %>% group_by(COUNTY) %>% mutate(minima = ggpmisc:::find_peaks(-mav, na.rm = TRUE))
-
-# get the delta start dates, if minimia = TRUE, get the earliest date, else (i.e all minima false) -> pick the date with the smallest value
-# for(county in counties){
-#   df <- filter(nc_dat, COUNTY == county)
-#   if(any(df$minima ==TRUE)){
-#     print(filter(df, minima == TRUE))
-#   }else{
-#     print(df)
-#   }
-# }
-# 
-
-
-
-
-# 
-# #sanity check - plot the minimum points via ggplot
-# for(county in counties){
-#   df <- filter(nc_dat, COUNTY == county)
-#   print(paste(county, sep=" : ",df$DATE[which(df$minima == TRUE)] ))
-#   c <- df$DATE[which(df$minima == TRUE)]
-#   print(c)
-#   q <- ggplot(df) + geom_col(aes(DATE, I), alpha = 0.7) +geom_line(aes(DATE, smoothed_inf), col = 'red') + ggtitle(county)
-#   print(q + geom_vline(xintercept = c))
-# }
